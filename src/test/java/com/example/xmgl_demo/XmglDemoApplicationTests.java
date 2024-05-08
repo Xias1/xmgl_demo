fd41d2f000a9c70d6c77df66ae6652897837fa50
Xias1 <2965237704@qq.com>
2024-05-08 16:58:18 +0800

    init

---
+++ src/test/java/com/example/xmgl_demo/XmglDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xmgl_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmglDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
